d6c1b672d4b48526037e95c4568d5433e2f5ac8a
舒波 郭 <2420019491@qq.com>
2019-11-15 23:37:43 +0800

    添加项目文件。

---
+++ _2019.11.15_2.py
@@ -0,0 +1,24 @@
+item_one=input()
+item_two=input()
+item_three=input()
+def method_name():#自定义函数
+    total=item_one+\
+          item_two+\
+          item_three
+    return total#将total值返回
+
+total = method_name()
+str=('adffdsf','dsfafdsa','afs',
+     'dffds')
+a=input()
+b=input()
+print(total,str,a+b)
+
+print("-------------------------------------------")
+
+list=['adfadsfs',1314,2.14,"ddfddss"]#列表
+typelist=['sdfas',1314]
+print(list)
+print(list[0:2])#从第一个数开始输出到第二个数
+print(list*2)
+print(list+typelist)#将两个列表连接起来
